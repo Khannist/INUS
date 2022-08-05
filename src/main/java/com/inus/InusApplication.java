21c1dde3aa6134c8e6a80ba9a774d259b9caa671
TrueDamage <thdclgus3@gmail.com>
2022-08-05 10:19:00 +0900

    no message

---
+++ src/main/java/com/inus/InusApplication.java
@@ -0,0 +1,29 @@
+package com.inus;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@SpringBootApplication
+public class InusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InusApplication.class, args);
+	}
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml");
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+	}
+}
